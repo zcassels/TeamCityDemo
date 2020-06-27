42e22d79f65d3be80bdc35c17357e8ebca259022
zcassels <zcassels@gmail.com>
2020-06-27 17:07:41 -0400

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -62,7 +62,7 @@ object Build : BuildType({
         }
         script {
             name = "NPM"
-            scriptContent = "npm whoops"
+            scriptContent = "npm whoopie"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
             dockerImage = "node"
         }
